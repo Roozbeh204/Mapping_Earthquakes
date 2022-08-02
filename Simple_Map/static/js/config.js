09dd64fd94825c61a5bea7cfa16f2de73bdfc570
Roozbeh Kargar <roozbehkargar60@gmail.com>
2022-08-01 22:04:34 -0400

    update

---
+++ Simple_Map/static/js/config.js
@@ -0,0 +1,2 @@
+// API key
+const API_KEY = "pk.eyJ1Ijoicm9vemJlaDIwNCIsImEiOiJjbDZiNXBscmIwMDFtM2tud2E1YmxxaGZvIn0.Mddxtcd0pgzKbFNs_w9N5w";
---
+++ config.js
@@ -0,0 +1,1 @@
+pk.eyJ1Ijoicm9vemJlaDIwNCIsImEiOiJjbDZiNXBscmIwMDFtM2tud2E1YmxxaGZvIn0.Mddxtcd0pgzKbFNs_w9N5w
